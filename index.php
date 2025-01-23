20f3344305472335b99dc400ebeda2189a85ec94
Benjamin Logan <benlogan414@gmail.com>
2025-01-22 18:26:48 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Benjamin's Webpage</title>
+</head>
+<body>
+    <header><h1>PHP Class - Benjamin's Website</h1></header>
+
+    <nav>
+        <ul>
+            <li></li>
+        </ul>
+    </nav>
+</body>
+</html>
