27fa7d70c47c50171ffbdb1cec81142f39bf3959
Benjamin Logan <benlogan414@gmail.com>
2025-03-26 19:47:06 -0500

    Added CodeIgniter framework

--- marathon/app/Config/Routes.php
+++ marathon/app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/play/(:any)', 'Home::play/$1');
+$routes->get('/admin', 'Admin::index');
--- marathon/app/Controllers/Home.php
+++ marathon/app/Controllers/Home.php
@@ -6,6 +6,13 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('homepage');
+    }
+
+    public function play($data): string
+    {
+        echo "Hello World " . $data;
+        exit();
+        //return view('welcome_message');
     }
 }
